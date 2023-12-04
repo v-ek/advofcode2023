d572c91af3b4d506edf9c71cf4a0d151fd615f28
Viktor Ek <viktor@theodoratech.se>
2023-12-04 23:12:25 +0100

    Solve day 3

---
+++ src/day3_1.py
@@ -0,0 +1,64 @@
+with open("inputs/day3_input.txt") as file:
+    lines = [line.strip() for line in file.readlines()]
+
+
+def compute_neighborhood(coordinates_list: list[tuple[int, int]], max_x: int, max_y: int):
+    neighborhood = set()
+    neighbors = (
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1), 
+        (1, -1),
+        (1, 0), 
+        (1, 1), 
+    )
+    for c_x, c_y in coordinates_list:
+        for d_x, d_y in neighbors:
+            new_coord = (c_x + d_x, c_y + d_y)
+            if new_coord[0] < 0 or new_coord[0] > max_x or new_coord[1] < 0 or new_coord[1] > max_y:
+                continue
+            else:
+                neighborhood.add(new_coord)
+    neighborhood -= set(coordinates_list)
+    return neighborhood
+
+part_numbers_to_sum = []
+special_char_coordinates = set()
+numbers_w_neighborhoods = []
+# cordinates to the left and down (left-handed?)
+for row_index in range(depth := len(lines)):
+    curr_digits = []
+    curr_number_coordinates = []
+    line = lines[row_index]
+    for col_index in range(width := len(line)): 
+        curr_coordinates = (col_index, row_index)
+        char = line[col_index]
+        if char.isdigit():
+            curr_digits.append(char)
+            curr_number_coordinates.append(curr_coordinates)
+        else:
+            if curr_number_coordinates:
+                curr_number = int("".join(curr_digits))
+                numbers_w_neighborhoods.append((curr_number, compute_neighborhood(curr_number_coordinates, width, depth)))
+                curr_digits = []
+                curr_number_coordinates = []
+            if char == ".":
+                continue
+            else:
+                special_char_coordinates.add(curr_coordinates)
+        # Special case, number at the edge
+        if col_index == width - 1:
+            if curr_number_coordinates:
+                curr_number = int("".join(curr_digits))
+                numbers_w_neighborhoods.append((curr_number, compute_neighborhood(curr_number_coordinates, width, depth)))
+                curr_digits = []
+                curr_number_coordinates = []
+
+for number, coordinates in numbers_w_neighborhoods:
+    if coordinates.intersection(special_char_coordinates):
+        part_numbers_to_sum.append(number)
+
+print(part_numbers_to_sum)
+print(sum(part_numbers_to_sum))
