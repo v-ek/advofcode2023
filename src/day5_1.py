ab4978c89fcb275fbae0c13339421de5f5522512
Viktor Ek <viktor@theodoratech.se>
2023-12-10 22:22:51 +0100

    Solve day 5 part 1 :rocket:

--- src/day5_1.py
+++ src/day5_1.py
@@ -1,29 +1,35 @@
-with open("inputs/day5_test.txt") as file:
+with open("inputs/day5_input.txt") as file:
     lines = [line.strip() for line in file.readlines()]
 
 maps = {}
-seeds = []
 seeds_with_paths = {}
+map_type = None
 for line in lines:
-    if line and line[0].isdigit():
-        dest_start, source_start, length = [int(entry) for entry in line.split(" ")]
-        for idx in range(length):
-            maps[map_type][source_start + idx] = dest_start + idx
-    elif line.startswith("seeds:"):
-        seeds = [int(number) for number in line.split(":")[1].split(" ") if number != ""]
-    elif line.startswith(" "):
-        continue
+    if line.startswith("seeds:"):
+        seeds_with_paths = {int(number): [int(number)] for number in line.split(":")[1].split(" ") if number != ""}
     elif line.endswith("map:"):
         map_type = line.split(" ")[0]
-        maps[map_type] = {}  # initialize an empty dict
+        maps[map_type] = []
+    elif line and line[0].isdigit():
+        maps[map_type].append(tuple([int(entry) for entry in line.split(" ")]))
+    elif not line and map_type:
+        for seed, path in seeds_with_paths.items():
+            for map in maps[map_type]:
+                if path[-1] >= map[1] and path[-1] <= map[1] + map[2]:
+                    mapped_value = path[-1] + map[0] - map[1]
+                    path.append(mapped_value)
+                    break
+            else:
+                path.append(path[-1])  # Maps to the same value
+else:
+    for seed, path in seeds_with_paths.items():
+        for map in maps[map_type]:
+            if path[-1] >= map[1] and path[-1] <= map[1] + map[2]:
+                mapped_value = path[-1] + map[0] - map[1]
+                path.append(mapped_value)
+                break
+        else:
+            path.append(path[-1])  # Maps to the same value
 
-seeds_with_paths = {}
-for seed in seeds:
-    seeds_with_paths[seed] = {}
-    previous_mapped_value = seed
-    for map_type, value_map in maps.items():
-        source_type, _, target_type = map_type.split("-")
-        seeds_with_paths[seed][target_type] = value_map.get(previous_mapped_value, previous_mapped_value)
-        previous_mapped_value = seeds_with_paths[seed][target_type]
-
-print(min((value["location"] for value in seeds_with_paths.values())))
+print(seeds_with_paths)
+print(min((path[-1] for path in seeds_with_paths.values())))
